639e773017c44f2fdbea44b06bc704168e757d66
Jairo Júnior <ojairo.dev@gmail.com>
2021-06-22 22:53:32 -0300

    create rooms

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,8 @@
+import {FormEvent, useState} from 'react'
 import {Link} from 'react-router-dom'
 import { Button } from '../components/Button'
 import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -9,6 +11,22 @@ import '../styles/auth.scss'
 
 export function NewRoom(){
   const {user} = useAuth()
+  const [newRoom, setNewRoom] = useState('')
+
+  async function handleCreateRoom(e: FormEvent){
+    e.preventDefault()
+
+    if(newRoom.trim() === ''){
+      return
+    }
+
+    const roomRef = database.ref('rooms')
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+
+  }
 
   return(
     <div id="page-auth">
@@ -21,14 +39,16 @@ export function NewRoom(){
         <div className= "main-content">
           <img src={logoImg} alt="LetMeAsk" />
           <h2>Criar uma nova sala</h2>
-          <form>
-          <input
-            type="text"
-            placeholder="Nome da sala"
-          />
-          <Button type="submit">
-            Criar sala
-          </Button>
+          <form onSubmit={e => handleCreateRoom(e)}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange = {e => setNewRoom(e.target.value)}
+              value={newRoom}
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
           </form>
           <p>
             Quer entrar em uma sala existente?
