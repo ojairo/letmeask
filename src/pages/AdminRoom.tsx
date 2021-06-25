2e3de5c3a5caa3796ccfde49eacc80c78bc0188f
Jairo Júnior <ojairo.dev@gmail.com>
2021-06-24 22:04:20 -0300

    create AdminRoom and change button styles

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,52 @@
+import {useParams} from 'react-router-dom'
+import { RoomCode } from '../components/RoomCode'
+import {Button} from '../components/Button'
+
+import logoImg from '../assets/images/logo.svg'
+import { Question } from '../components/Question'
+import { useRoom } from '../hooks/useRoom'
+import '../styles/room.scss'
+
+type ParamsProps = {
+  id: string
+}
+
+export function AdminRoom(){
+  const params = useParams<ParamsProps>()
+  const roomId = params.id
+
+  const {title, questions} = useRoom(roomId)
+
+  return(
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code= {params.id}/>
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map(question => {
+            return(
+              <Question
+                key = {question.id}
+                content = {question.content}
+                author = {question.author}
+              />
+            )
+          })}
+        </div>
+      </main>
+    </div>
+  )
+}
